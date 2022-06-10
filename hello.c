f76df0f9d2c68d38df04bb05d445ceadfd1acc72
Zach Ho <zachtcho@users.noreply.github.com>
2022-06-11 00:35:03 +0800

    Update hello.c
    add a sub-routine "plus"

--- hello.c
+++ hello.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+int plus (int a, b) {
+    int c;
+    c = a + b;
+    return c;
+}
+
 int main () {
+    int sum;
     printf("Hello world1!\n");
+    sum = plus(1, 2);
+    printf("sum = %d\n", sum);
     return 0;
 }
